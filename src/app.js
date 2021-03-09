106505659f9467887fda5bc0384dd7bee87e23ec
abhay376 <abhay100702@gmail.com>
2021-03-09 18:53:06 +0530

    Deployment 1

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@
 
 
 const express = require("express");
-const mongo = require("./mongo");
 const bodyParser = require("body-parser")
  const cors= require('cors');
 // const session = require('express-session')
